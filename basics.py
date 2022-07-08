748061b3d2f71d4387f2ff8e4f4812cf9bd15090
Jack Pena <penajackc@gmail.com>
2022-07-08 10:37:37 -0700

    completed python basics

--- basics.py
+++ basics.py
@@ -40,3 +40,34 @@ for letter in some_list:
             res.append(letter)
 
 print(res)
+
+def checkDriverAge(age = 0):
+
+    if int(age) < 18:
+        print("Sorry, you are too young to drive this car. Powering off")
+    elif int(age) > 18:
+        print("Powering On. Enjoy the ride!");
+    elif int(age) == 18:
+        print("Congratulations on your first year of driving. Enjoy the ride!")
+
+
+
+#1. Wrap the above code in a function called checkDriverAge(). Whenever you call this function, you will get prompted for age. 
+# Notice the benefit in having checkDriverAge() instead of copying and pasting the function everytime?
+
+#2 Instead of using the input(). Now, make the checkDriverAge() function accept an argument of age, so that if you enter:
+# checkDriverAge();
+#it returns "Powering On. Enjoy the ride!"
+#also make it so that the default age is set to 0 if no argument is given.
+
+#highest even
+
+def highest_even(li):
+    evens = []
+    for item in li:
+        if item % 2 == 0:
+            evens.append(item)
+    
+    return max(evens)
+
+print(highest_even([10,2,3,4,8,11]))
