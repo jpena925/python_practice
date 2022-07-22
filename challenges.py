005845534787f070a3ec1d7587d7ec5f1ec2e5c2
Jack Pena <penajackc@gmail.com>
2022-07-22 08:51:55 -0700

    day2 challenge

--- challenges.py
+++ challenges.py
@@ -3,3 +3,13 @@
 for i in range(2000,3201):
     if i % 7 == 0 and i % 5 != 0:
         print(i, end=',')
+
+#Day 2
+#Write a program which can compute the factorial of a given numbers.The results should be printed in a comma-separated sequence on a single line.Suppose the following input is supplied to the program: 8
+# Then, the output should be:40320_**
+
+def factorial(num):
+    if(num == 1):
+        return 1
+    
+    return factorial(num - 1) * num
