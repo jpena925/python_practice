3c094d63445a5f526f1591d40a2204c5d9690231
Jack Pena <penajackc@gmail.com>
2022-07-28 14:39:06 -0700

    unittest

---
+++ testing/script.py
@@ -0,0 +1,26 @@
+from random import randint
+import sys
+# generate a number 1~10
+def run_guess(guess, answer):
+    if  0 < guess < 11:
+        if guess == answer:
+            print('you are a genius!')
+            return True
+    else:
+        print('hey bozo, I said 1~10')
+        return False
+
+answer = randint(1, 10)
+
+# input from user?
+# check that input is a number 1~10
+if __name__ == '__main__':
+    while True:
+        try:
+            guess = int(input('guess a number 1~10:  '))
+            if (run_guess(guess, answer)):
+                break
+            
+        except ValueError:
+            print('please enter a number')
+            continue
