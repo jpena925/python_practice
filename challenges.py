f2717880586eb540931887b685835dd90a3c3f02
Jack Pena <penajackc@gmail.com>
2022-07-29 14:10:36 -0700

    Day 2 challenges

--- challenges.py
+++ challenges.py
@@ -25,3 +25,85 @@ def num_dict(num):
     for i in range(1, num+1):
         res[i] = i * i
     return res
+
+
+
+#day 2a
+
+#Write a program which accepts a sequence of comma-separated numbers from console and generate a list and a 
+# tuple which contains every number.Suppose the following input is supplied to the program:
+
+# 34,67,55,33,12,98
+
+# Then, the output should be:
+
+
+# ['34', '67', '55', '33', '12', '98']
+# ('34', '67', '55', '33', '12', '98')
+
+def day2a():
+    l = input("please enter a list a numbers").split(',')
+    t = tuple(l)
+    print(l, t)
+    
+
+#Day 2b
+
+# Define a class which has at least two methods:
+
+# getString: to get a string from console input
+# printString: to print the string in upper case.
+
+# Also please include simple test function to test the class methods.
+
+### Hints:
+
+#Use **init** method to construct some parameters_
+
+class Day2b(object):
+    def __init__(self):
+        self.s = ""
+
+    def get_string(self):
+        self.s = input("please enter a string  ")
+
+    def print_string(self):
+        print(self.s.upper())
+
+
+# test1 = Day2b()
+# test1.get_string()
+# test1.print_string()
+
+        
+
+
+
+
+
+
+
+
+
+### **Question:**
+
+# Write a program that calculates and prints the value according to the given formula:
+
+# Q = Square root of [(2 _ C _ D)/H]
+
+# Following are the fixed values of C and H:
+# C is 50. H is 30.
+
+# D is the variable whose values should be input to your program in a comma-separated sequence.For example
+# Let us assume the following comma separated input sequence is given to the program:
+# 100,150,180
+
+# The output of the program should be:
+# 18,22,24
+from math import sqrt
+
+C = 50
+H = 30
+
+def math_prob(D):
+    return sqrt((2 * C * D)/ H)
