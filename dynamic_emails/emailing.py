e0e7a04b61b89e4c3d73973e985d956b33710a34
Jack Pena <penajackc@gmail.com>
2022-08-26 08:53:04 -0700

    dynamic emailing

--- dynamic_emails/emailing.py
+++ dynamic_emails/emailing.py
@@ -4,13 +4,17 @@
 import smtplib
 
 from email.message import EmailMessage
+from string import Template
+from pathlib import Path #similar to os.path
 
+html = Template(Path('index.html').read_text())
 email = EmailMessage()
 email['from'] = 'Jack Pena'
 email['to'] = 'dummyemail@gmail.com'
 email['subject'] = 'Dummy subject line'
 
-email.set_content('Content goes here')
+#here you can set the name variable that is in index.html 
+email.set_content(html.substitute(name='TinTin') , 'html')
 
 with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
     smtp.ehlo()
